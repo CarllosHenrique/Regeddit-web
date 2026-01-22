37942838807561505fa03cd0ecc43a2f2642c1ee
CarllosHenrique <crick.lucas@gmail.com>
2026-01-21 23:03:02 -0300

    feat: implement mailer layout and styles, enhance user password and unlock flows

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
   devise_for :users, controllers: {
     sessions: "users/sessions",
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    passwords: "users/passwords"
   }
 end
